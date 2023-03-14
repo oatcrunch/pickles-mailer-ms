6dfad6248f5a3f6d619c6420a963253c0e9cb5fb
Melvin Liew <melvin@testemail.com>
2023-03-14 20:23:24 +0800

    mailer-service: add publishing event functionality.

---
+++ modules/mailer-service/src/event-publisher.ts
@@ -0,0 +1,12 @@
+import * as dotEnv from 'dotenv';
+import { EventBridgeClient } from '@aws-sdk/client-eventbridge';
+
+dotEnv.config();
+
+export const ebClient = new EventBridgeClient({
+    region: process.env.AWS_BUCKET_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    }
+});

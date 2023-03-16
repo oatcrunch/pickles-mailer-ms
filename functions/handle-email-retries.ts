811be504b7f0af904f3401679c91e045aa829f0f
Melvin Liew <melvin@testemail.com>
2023-03-16 16:03:31 +0800

    cloud-infra: add algo comments to handle email retries.

--- functions/handle-email-retries.ts
+++ functions/handle-email-retries.ts
@@ -7,7 +7,7 @@ export const main = async (
     console.log('event 👉', event);
     try {
         const apiGatewayEvent = <APIGatewayEvent>event;
-        await processBody(<string>apiGatewayEvent.body);
+        return await processBody(<string>apiGatewayEvent.body);
         throw new Error(`Error sending enquiry with event: ${event}`);
     } catch (err: any) {
         console.error(
@@ -25,6 +25,12 @@ export const main = async (
 };
 
 const processBody = async (body: string) => {
+    // 1. look for db rows based on email transaction id
+    // 2. if count is more than 3 then return saying exceeded retries, else proceed with step 3
+    // 3. if count is less than 3 and if upload transaction id is not empty, then list objects from s3 based on that id
+    // 4. with the email content and optionally attachments from s3, retry sending email
+    // 5a. if email sent successful then return saying succeeded
+    // 5b. if email sent failed then trigger event for retry after x seconds delay
     return {
         statusCode: 200,
         body: JSON.stringify({
